c1c08736a59a90fddbc63d2e630664f878db47a7
Karim Adahchour - Gpi Group <adahchour@gpi-tanks.com>
2026-05-29 23:51:51 +0200

    Add app files

---
+++ tailwind.config.ts
@@ -0,0 +1,1 @@
+﻿import type { Config } from "tailwindcss"; const config: Config = { content: ["./app/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"], theme: { extend: { colors: { "maroc-red": "#C1272D", "maroc-green": "#006233" } } }, plugins: [] }; export default config;
